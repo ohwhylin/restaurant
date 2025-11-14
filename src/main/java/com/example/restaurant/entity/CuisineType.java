481fad1e07cbdcd191fd5d5d5099ec9bdd2b3185
Полина Чубыкина <polinakill04@gmail.com>
2025-11-14 21:55:41 +0400

    добавила несколько классов

---
+++ src/main/java/com/example/restaurant/entity/CuisineType.java
@@ -0,0 +1,9 @@
+package com.example.restaurant.entity;
+
+public enum CuisineType {
+    EUROPEAN,
+    ITALIAN,
+    CHINESE,
+    JAPANESE,
+    VIETNAMESE
+}
---
+++ src/main/java/com/example/restaurant/entity/Review.java
@@ -0,0 +1,16 @@
+package com.example.restaurant.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    private Long visitorId;
+    private Long restaurantId;
+    private int score;
+    private String text;
+}
