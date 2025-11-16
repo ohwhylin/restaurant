cd54751ca2782723379264ed5cbb351db2ded0d5
Полина Чубыкина <polinakill04@gmail.com>
2025-11-16 15:37:51 +0400

    готовая 5 практика
    # Conflicts:
    #	src/main/java/com/example/restaurant/entity/Visitor.java

---
+++ src/main/java/com/example/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,25 @@
+package com.example.restaurant.repository;
+
+import com.example.restaurant.entity.Restaurant;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RestaurantRepository {
+
+    private final List<Restaurant> restaurants = new ArrayList<>();
+
+    public void save(Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
+
+    public void remove(Restaurant restaurant) {
+        restaurants.remove(restaurant);
+    }
+
+    public List<Restaurant> findAll() {
+        return restaurants;
+    }
+}
